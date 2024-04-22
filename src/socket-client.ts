eec2a259caf53d7b7cc18a9fcdda6732526d996b
julioD <juliougd@hotmail.com>
2024-04-22 13:20:24 -0300

    cambia base url

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,13 +1,18 @@
 import { Manager, Socket } from "socket.io-client";
+let socket:Socket;
+export const connectToServer = (token:string) => {
+  const manager = new Manager("http://teslo-api-production.up.railway.app/socket.io/socket.io.js",{
+    extraHeaders:{
+      authentication:token.trim()
+    }
+  });
+  socket?.removeAllListeners();
+  socket = manager.socket("/");
 
-export const connectToServer = () => {
-  const manager = new Manager("http://teslo-api-production.up.railway.app/socket.io/socket.io.js");
-
-  const socket = manager.socket("/");
-  addListeners(socket);
+  addListeners();
 };
 
-const addListeners = (socker: Socket) => {
+const addListeners = () => {
   const serverStatusLabel = document.querySelector("#server-status")!;
   let clientsUl = document.querySelector("#client-ul")!;
 
@@ -15,15 +20,15 @@ const addListeners = (socker: Socket) => {
   const messageForm = document.querySelector<HTMLFormElement>("#message-form")!;
   const messagesUl = document.querySelector<HTMLUListElement>("#messages-ul")!;
 
-  socker.on("connect", () => {
+  socket.on("connect", () => {
     serverStatusLabel.innerHTML = "Online";
   });
 
-  socker.on("disconnect", () => {
+  socket.on("disconnect", () => {
     serverStatusLabel.innerHTML = "Offline";
   });
 
-  socker.on("clients-updated", (clients: string[]) => {
+  socket.on("clients-updated", (clients: string[]) => {
     let clientHTML = "";
     clients.forEach((clientid) => {
       clientHTML += `
@@ -38,17 +43,17 @@ const addListeners = (socker: Socket) => {
     event.preventDefault();
     if (messageInput.value.trim().length <= 0) return;
 
-    socker.emit("message-from-client", { id: "YO!!", message: messageInput.value });
+    socket.emit("message-from-client", { id: "YO!!", message: messageInput.value });
     messageInput.value = "";
   });
 
-  socker.on("message-from-server", (payload: { fullname: string; message: string }) => {
+  socket.on("message-from-server", (payload: { fullname: string; message: string }) => {
     console.log(payload);
 
     const newMessage = `
     <li>
     <strong>
-    ${payload.fullname}
+    ${payload.fullname}: 
     </strong>
     <span>
     ${payload.message}
